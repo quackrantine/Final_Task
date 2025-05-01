57767fc387c277fd7afd9e4341267cc7a2a8ad13
quackrantine <153075297+quackrantine@users.noreply.github.com>
2025-05-01 10:35:25 +0800

    Update db4.php

--- db.php
+++ db.php
@@ -2,8 +2,8 @@
 
 function getDBConnection() {
     $servername = "sql.freedb.tech";
-    $username = "freedb_quackratine"; // Make sure this matches FreeDB EXACTLY
-    $password = "!S$62Am!DTx*2ZF";    // Make sure this matches FreeDB EXACTLY
+    $username = "freedb_finaltask"; // Make sure this matches FreeDB EXACTLY
+    $password = "c%?Yy6@y7hvKbzS";    // Make sure this matches FreeDB EXACTLY
     $database = "freedb_finaltask";   // Use this variable below
     $port = 3306;                     // FreeDB uses port 3306
 
